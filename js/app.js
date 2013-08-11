c3585490013295d82144ef5a9d820261e6d61bd5
Dave Kinkead <dave@kinkead.com.au>
2013-08-11 20:27:20 +1000

    Merge branch 'master' of https://github.com/davekinkead/unicorns
    Conflicts:
    	index.html

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@ function fartCtrl($scope) {
 	$scope.fart = function(note) {
 		console.log('farting');
 		// bind argument to file here
-		var audio = 'audio/test.ogg';
+		var audio = document.getElementById("test");
+    audio.load();
 		audio.play();
 	}
 }
